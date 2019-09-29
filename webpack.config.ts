4704a5966e0d130d3db80fddf4704863de5ed12a
Andrey Pakhomov <stonewalrusfan@gmail.com>
2019-09-29 15:53:06 +0300

    webpack css

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* tslint:disable:object-literal-sort-keys */
-import * as path from 'path';
+import path from 'path';
 import webpack from 'webpack';
 import webpackDevServer from 'webpack-dev-server';
+
+const isDevelopment = true;
 
 const root = (args: string): string => path.join(...[__dirname].concat('./', args));
 
@@ -11,11 +13,11 @@ const webpackConfig = {
     bundle: [root('src/index.tsx')]
   },
   output: {
-    filename: 'js/main.[hash].js',
-    path: root('build/')
+    filename: 'js/main.js',
+    path: root('public/')
   },
   devServer: {
-    contentBase: path.join(__dirname, 'build'),
+    contentBase: path.join(__dirname, 'public'),
     compress: true,
     port: 8080,
     historyApiFallback: true,
@@ -28,6 +30,29 @@ const webpackConfig = {
         test: /\.(js|jsx|tsx|ts)$/,
         exclude: /node_modules/,
         loader: ['babel-loader', 'ts-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: `file-loader`,
+            options: {
+              name: `css/[name].css`
+            }
+          },
+          {
+            loader: `extract-loader`
+          },
+          {
+            loader: `css-loader?-url`
+          },
+          {
+            loader: `postcss-loader`
+          },
+          {
+            loader: `sass-loader`
+          }
+        ]
       }
     ]
   },
@@ -46,7 +71,7 @@ const webpackConfig = {
     },
     extensions: ['.ts', '.tsx', '.js', 'json']
   },
-  plugins: [],
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   devtool: 'source-map'
 };
 
